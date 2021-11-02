fe67260f4460cb8cd55fea8356828ad29670f8c9
Michael Goncalves <michael.goncalves@unimedbh.com.br>
2021-11-02 12:07:46 -0300

    Day 0 of the 100 days of code - python

---
+++ Python/DAY-0/Exercise-0 -0 .py
@@ -0,0 +1,9 @@
+#Learn about how string works in python
+print("Day 1 -  Python print function")
+print ("The Function is declared like this:")
+print ("print('whats to print?')")
+
+#to concat string use the "+" like this
+print("hello" + " world")
+
+#to code in python don use unnecessary space.
